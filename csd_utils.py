9808607074d87ebc5641b0e8ab7aa64da1f54899
Philippe Rigaux <rigaux@users.noreply.github.com>
2026-01-28 21:39:42 +0100

    Working on production of the full report

--- csd_utils.py
+++ csd_utils.py
@@ -28,6 +28,10 @@ OUT_DIR="results"
 PREDICTED_DIR="predicted"
 GROUND_TRUTH_DIR="ground_truth"
 
+SINGLE_SCORE_ACTION="single"
+ALL_SCORES_ACTION="all" 
+BUILD_FULL_REPORT="report" 
+
 def main(argv=None):
 	"""
 	Slightly adapted from the musicdiff code by Francesco Foscarion / Greg Chapman 
@@ -47,13 +51,14 @@ def main(argv=None):
                    help="Action: 'single' or 'multiple' (score(s))")
 	args = parser.parse_args()
 	
-	if args.action == "single":
-		
+	if args.action == SINGLE_SCORE_ACTION:
 		if args.score is None:
 			sys.exit ("You must provide a file name")
 		compare_single_score (args.score)
-	elif args.action == "multiple":
-		compare_multiple_scores ()
+	elif args.action == ALL_SCORES_ACTION:
+		compare_collection ()
+	elif args.action == BUILD_FULL_REPORT:
+		build_full_report ()
 	else:
 		print (f"Unknown action {args.action}")
 
@@ -62,17 +67,17 @@ def main(argv=None):
 def compare_single_score(score_name):
 	"""
 	   Compares a predicted score (in predicted)
-	   and a griund truth (in ground_truth)
+	   and a ground truth (in ground_truth)
 	"""
 	predicted_path = f"predicted/{score_name}"
 	ground_truth_path = f"ground_truth/{score_name}"
 	
 	if not os.path.exists(predicted_path):
-		sys.exit (f"File {predicted_path}  does not exist. Please check.")
+		raise Exception (f"File {predicted_path}  does not exist. Please check.")
 	if not os.path.isfile(predicted_path):
-		sys.exit (f"{predicted_path} is not a file. Please check.")
+		raise Exception (f"{predicted_path} is not a file. Please check.")
 	if not os.path.exists(ground_truth_path):
-		sys.exit (f"{ground_truth_path}  does not exist. Please check.")
+		raise Exception (f"{ground_truth_path}  does not exist. Please check.")
 		
 	try:
 		scpath = Path(score_name)
@@ -97,7 +102,8 @@ def compare_single_score(score_name):
 
 	oplist = []
 	for diff in diff_list:
-		oplist.append({"op": diff[0], "cost": diff[3]})
+		oplist.append({"op": diff[0],"x1":  str(diff[1]), "x2": str(diff[2]),
+						"cost": diff[3]})
 	report = {"cost": _cost, "nb_diffs": len(diff_list), 
 					"operations": oplist}
 
@@ -123,10 +129,25 @@ def compare_single_score(score_name):
 			print_text_output=True, 
 			detail=detail)
 
-def compare_multiple_scores():
+def compare_collection():
 	"""
-	   Compares a predicted score (in predicted)
-	   and a ground truth (in ground_truth)
+	   Loop on all scores and compute diffs
+	"""
+	with open("dataset.json") as json_data:
+		dataset = json.load (json_data)
+	
+		# Loop on the scores, show the results
+		for score in dataset["list_opus"]:
+			file_name = f"{score['ref']}.mei"
+			print (f"\n\nCompute DIFFS for score {score['title']} (file {file_name})")
+			try:
+				compare_single_score (file_name)
+			except Exception as e:
+				print (f"Exception met for score {score['title']}: {e}")
+
+def ml_training():
+	"""
+	   Run the ML training function
 	"""
 	
 	# Good enough for the time being
@@ -134,6 +155,41 @@ def compare_multiple_scores():
 
 	mdiff.diff_ml_training("predicted", "ground_truth", 
 				"results", detail=detail)
+
+
+def build_full_report():
+	"""
+	  Build HTML and other file summarizing the comparison results
+	"""
+	
+	# First load the dataset.json 
+	with open("dataset.json") as json_data:
+		dataset = json.load (json_data)
+	with open("full_report.html", "w") as res_file:
+		res_file.write (f"<table border='1'>")
+		res_file.write ("<tr>")
+		res_file.write (f"<th>Ref</th><th>Title</th><th>Images</th>")
+		res_file.write (f"<th>Nb diffs</th>")
+		res_file.write (f"</tr>")
+
+		# Loop on the scores, show the results
+		for score in dataset["list_opus"]:
+			res_file.write (f"<tr>")
+			res_file.write (f"<td>{score['ref']}</td>")
+			res_file.write (f"<td>{score['title']}</td>")
+			res_file.write (f"<td><a target='_blank' href='{score['iiif_link']}'>link</a></td>")
+
+			# Is there a result file ?
+			report_file =f"results/{score['ref']}_report.json"
+			if os.path.exists(report_file):
+				print(f"Result file exists for score {score['ref']}.")
+				with open(report_file, "r") as report_file:
+					report = json.load (report_file)
+					res_file.write (f"<td>{report['nb_diffs']}</td>")
+			res_file.write (f"</tr>")
+		
+		res_file.write (f"</table>")
+
 
 def old_decomposed_code():
 		
